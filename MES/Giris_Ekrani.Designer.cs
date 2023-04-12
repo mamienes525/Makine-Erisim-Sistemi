2700f9a2824f6008b730e36a3160ada4823b2de7
mamienes525 <99298402+mamienes525@users.noreply.github.com>
2023-04-12 23:13:17 +0300

    Giriş ekranı güncellendi

--- MES/Giris_Ekrani.Designer.cs
+++ MES/Giris_Ekrani.Designer.cs
@@ -31,19 +31,20 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giris_Ekrani));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.kapatD = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.KullaniciAdi = new System.Windows.Forms.TextBox();
+            this.Sifre = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(582, -6);
+            this.pictureBox1.Location = new System.Drawing.Point(523, -25);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(525, 490);
+            this.pictureBox1.Size = new System.Drawing.Size(652, 573);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -51,44 +52,52 @@
             // 
             // kapatD
             // 
+            this.kapatD.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.kapatD.BackColor = System.Drawing.Color.Transparent;
             this.kapatD.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("kapatD.BackgroundImage")));
             this.kapatD.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.kapatD.FlatAppearance.BorderSize = 0;
             this.kapatD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.kapatD.Location = new System.Drawing.Point(1307, 12);
+            this.kapatD.Location = new System.Drawing.Point(1327, 12);
             this.kapatD.Name = "kapatD";
             this.kapatD.Size = new System.Drawing.Size(47, 43);
             this.kapatD.TabIndex = 1;
             this.kapatD.UseVisualStyleBackColor = false;
             this.kapatD.Click += new System.EventHandler(this.kapatD_Click);
             // 
-            // textBox1
+            // KullaniciAdi
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(719, 339);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 31);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Kullanıcı Adı veya Eposta";
+            this.KullaniciAdi.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.KullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.KullaniciAdi.Location = new System.Drawing.Point(719, 350);
+            this.KullaniciAdi.Name = "KullaniciAdi";
+            this.KullaniciAdi.Size = new System.Drawing.Size(259, 31);
+            this.KullaniciAdi.TabIndex = 2;
+            this.KullaniciAdi.Text = "\r\nKullanıcı Adı veya E-Posta";
+            this.KullaniciAdi.Enter += new System.EventHandler(this.KullaniciAdi_Enter);
+            this.KullaniciAdi.Leave += new System.EventHandler(this.KullaniciAdi_Leave);
             // 
-            // textBox2
+            // Sifre
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox2.Location = new System.Drawing.Point(719, 396);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(259, 31);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Şifre";
+            this.Sifre.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Sifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sifre.Location = new System.Drawing.Point(719, 396);
+            this.Sifre.Name = "Sifre";
+            this.Sifre.Size = new System.Drawing.Size(259, 31);
+            this.Sifre.TabIndex = 3;
+            this.Sifre.Text = "Şifre";
+            this.Sifre.Enter += new System.EventHandler(this.Sifre_Enter);
+            this.Sifre.Leave += new System.EventHandler(this.Sifre_Leave);
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(719, 452);
+            this.button1.Location = new System.Drawing.Point(770, 447);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(259, 32);
+            this.button1.Size = new System.Drawing.Size(159, 32);
             this.button1.TabIndex = 4;
             this.button1.Text = "Giriş Yap";
             this.button1.UseVisualStyleBackColor = false;
@@ -104,8 +113,8 @@
             this.ClientSize = new System.Drawing.Size(1386, 788);
             this.ControlBox = false;
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sifre);
+            this.Controls.Add(this.KullaniciAdi);
             this.Controls.Add(this.kapatD);
             this.Controls.Add(this.pictureBox1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
@@ -116,6 +125,7 @@
             this.Name = "Giris_Ekrani";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "GirisEkrani";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Giris_Ekrani_Load);
             this.Layout += new System.Windows.Forms.LayoutEventHandler(this.Giris_Ekrani_Layout);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -128,8 +138,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button kapatD;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox KullaniciAdi;
+        private System.Windows.Forms.TextBox Sifre;
         private System.Windows.Forms.Button button1;
     }
 }
--- MES/Giris_Ekrani.cs
+++ MES/Giris_Ekrani.cs
@@ -7,16 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MES
 {
     public partial class Giris_Ekrani : Form
     {
+        
         public Giris_Ekrani()
         {
             InitializeComponent();
         }
-
+      
         private void Giris_Ekrani_Load(object sender, EventArgs e)
         {
 
@@ -31,6 +33,30 @@ namespace MES
         private void Giris_Ekrani_Layout(object sender, LayoutEventArgs e)
         {
 
+        }
+
+        private void KullaniciAdi_Enter(object sender, EventArgs e)
+        {
+            if (KullaniciAdi.Text == "\r\nKullanıcı Adı veya E-Posta")
+            KullaniciAdi.Text = "";
+        }
+
+        private void KullaniciAdi_Leave(object sender, EventArgs e)
+        {
+            if (KullaniciAdi.Text == "")
+                KullaniciAdi.Text = "\r\nKullanıcı Adı veya E-Posta";
+        }
+
+        private void Sifre_Enter(object sender, EventArgs e)
+        {
+            if (Sifre.Text == "Şifre")
+                Sifre.Text = "";
+        }
+
+        private void Sifre_Leave(object sender, EventArgs e)
+        {
+            if (Sifre.Text == "")
+                Sifre.Text = "Şifre";
         }
     }
 }
